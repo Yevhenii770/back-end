c0cb94e034cbbbf8cec3ca4750888f224a81074a
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-12-12 21:15:45 +0100

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,11 +1,11 @@
 const cron = require("cron");
-const https = require("https");
+const https = require("node:https");
 const { BACKEND_URL } = process.env;
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
     .get(BACKEND_URL, (res) => {
-      if (res.statusCode === 200) {
+      if (res.statusCode == 200) {
         console.log("Server restarted");
       } else {
         console.error(

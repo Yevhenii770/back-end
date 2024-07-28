98eab29c3c6aa14c77212176bb25549e53e756e6
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2024-07-28 13:23:26 -0400

    update message

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: "Server is running / 404" });
 });
 
 app.use((err, req, res, next) => {
